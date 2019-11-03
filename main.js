80df0250cb0133e527a1947280c44c53effc1dab
Jacob Malinsky <jacobmalinsky@Jacobs-MacBook-Pro.local>
2019-11-03 16:12:00 +0200

    Practice for calc

---
+++ main.js
@@ -0,0 +1,18 @@
+alert("welcome to my calculator :D ");
+var x= parseInt(prompt("enter a number"));
+var y=parseInt(prompt("enter an other number"));
+var z= x+y
+alert("your answer is "+z)
+
+alert("time to subtract!")
+z = x - y;
+alert("your answer is ... " + z)
+
+alert("time to multiply!")
+z = x * y;
+alert("your answer is ... " + z)
+
+alert("time to divide!")
+z = x / y;
+alert("your answer is ... " + z)
+
